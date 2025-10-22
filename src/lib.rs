53c7bbf4e2801487a03c5bea24d9e297eef33225
philo-groves <philogroves@gmail.com>
2025-10-21 22:52:08 -0400

    Remove klib (now resides in ktest)

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,3 @@ pub fn ktest(attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> pr
     // Therefore, we delegate the implementation to its interior `ktest` module.
     macros::ktest::ktest(attr, item)
 }
-
-/// `#[klib]` attribute macro
-#[proc_macro_attribute]
-pub fn klib(attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    // Due to Rust constraints, procedural macros must be defined in the root of the crate.
-    // Therefore, we delegate the implementation to its interior `klib` module.
-    macros::klib::klib(attr, item)
-}
--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,2 +1,1 @@
-pub mod klib;
 pub mod ktest;
